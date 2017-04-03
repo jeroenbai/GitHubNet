b8de8323311313a202f7bc07c60efb03e4989077
Jeroen Baidenmann <info@devmobile.nl>
2017-04-03 16:15:53 +0200

    My commit message

---
+++ Backup/About.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GitHubNet
+{
+    public partial class About : System.Web.UI.Page
+    {
+        protected string testContent;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            testContent = "Hello GitHub.";
+        }
+    }
+}
